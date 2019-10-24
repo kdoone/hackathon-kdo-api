1748327734c5a0b20fbb14167b6bf9aa792e24e6
Sanzhar <s.shakhmerden@muratov.kz>
2019-10-24 16:08:46 +0600

    added type to clean unceseray

--- src/util/cleanUnessearyFromValidation.ts
+++ src/util/cleanUnessearyFromValidation.ts
@@ -1,4 +1,4 @@
-export const cleanUnnecessary = (arr) => {
+export const cleanUnnecessary = (arr: any) => {
     return arr.map(({ value, location, ...cleanedItem }: any) => {
         const { msg: { statusCode, message }, param } = cleanedItem;
         return {
