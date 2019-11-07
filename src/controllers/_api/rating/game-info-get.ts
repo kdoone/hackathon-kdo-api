db758e03948c3b06f0e006deacd08659ab367ad5
Sanzhar <s.shakhmerden@muratov.kz>
2019-11-07 16:32:00 +0600

    league star and updated achievements

--- src/controllers/_api/rating/game-info-get.ts
+++ src/controllers/_api/rating/game-info-get.ts
@@ -4,11 +4,13 @@ export const gameInfoTotal = async (req: any, res: Response, next: NextFunction)
     try {
 
         res.json({
+            league: req.league,
+            star: req.star,
+            achievements: req.achievements,
             worldRecords: req.worldRecords,
             myWorldRecord: req.myWorldRecord,
             friendRecords: req.friendRecords,
             myFriendRecord: req.myFriendRecord,
-            achievements: req.achievements
         });
 
     }
--- src/middlewares/get-achievements.ts
+++ src/middlewares/get-achievements.ts
@@ -5,29 +5,48 @@ export const getAchievementsMiddleware = async (req: any, res: Response, next: N
     try {
         const { id: myUserId } = req.user;
 
-        const { records } = await User.findById(myUserId);
+        const { records } = await User.findById(myUserId).populate('records');
 
-        let achievements = [];
+        let achievements: any = {
+            fastHand: false,
+            goodMemory: false,
+            attentiveness: false,
+            bronze: false,
+            silver: false,
+            gold: false,
+            platinum: false
+        };
 
-        if (records.shulteTable >= 15000) {
-            achievements.push({ name: 'fastHand', verity: true });
-        } else {
-            achievements.push({ name: 'fastHand', verity: false });
-        }
+        if (records.shulteTable >= 15000) { achievements.fastHand = true; }
+        if (records.rememberNumber >= 1900 || records.rememberWords >= 1900 || records.memorySquare >= 5000) { achievements.goodMemory = true; }
+        if (records.coloredWords >= 2000) { achievements.attentiveness = true; }
 
-        if (records.rememberNumber >= 1900 || records.rememberWords >= 1900 || records.memorySquare >= 5000) {
-            achievements.push({ name: 'goodMemory', verity: true });
-        } else {
-            achievements.push({ name: 'goodMemory', verity: false });
-        }
+        const { totalRecord } = req.myWorldRecord;
 
-        if (records.coloredWords >= 2000) {
-            achievements.push({ name: 'attentiveness', verity: true });
-        } else {
-            achievements.push({ name: 'attentiveness', verity: false });
-        }
+        if (totalRecord >= 0) { req.league = 'bronze'; req.star = 1; }
+        if (totalRecord >= 10000) { req.star = 2; }
+        if (totalRecord >= 15000) { req.star = 3; }
 
-        req.achievements = achievements;
+        if (totalRecord >= 20000) { req.league = 'silver'; achievements.bronze = true; req.star = 1; }
+        if (totalRecord >= 25000) { req.star = 2; }
+        if (totalRecord >= 30000) { req.star = 3; }
+
+        if (totalRecord >= 40000) { req.league = 'gold'; achievements.silver = true; req.star = 1; }
+        if (totalRecord >= 45000) { req.star = 2; }
+        if (totalRecord >= 50000) { req.star = 3; }
+
+        if (totalRecord >= 60000) { req.league = 'platinum'; achievements.gold = true; req.star = 1; }
+        if (totalRecord >= 65000) { req.star = 2; }
+        if (totalRecord >= 70000) { req.star = 3; }
+
+        if (totalRecord >= 80000) { achievements.platinum = true; }
+
+        const achievementsArr = Object.keys(achievements).map(item => ({
+            name: item,
+            verity: achievements[item]
+        }));
+
+        req.achievements = achievementsArr;
 
         next();
 
